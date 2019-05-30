033639d03f2f848be80985ee7207a32c6d09c021
pablokwiatkowski <pawuskwiatkowski@gmail.com>
2019-05-31 00:43:33 +0200

    Nowe linijki kodu.

--- obstacle.py
+++ obstacle.py
@@ -10,3 +10,11 @@ class Obstacle:
     def __init__(self):
         self.obs1 = pygame.image.load('walls2.png').convert()
         self.obs2 = pygame.image.load('walls3.png').convert()
+
+    def draw(self):
+        self.display = pygame.display.get_surface()
+        self.display.blit(self.obs1,0,0)
+        self.display.blit(self.obs2,0,0)
+
+    def do(self):
+        self.draw()
